2b52348de214ae712e2465e62f79bd6c698a9210
Mufanc <mufanc.xyz@gmail.com>
2023-09-02 02:09:34 +0800

    feat: 添加页码

---
+++ global-top.vue
@@ -0,0 +1,28 @@
+<template>
+    <span class="cur-page" v-show='showPage'>{{ page }}</span>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+// @ts-ignore
+const count = $slidev.nav.total
+
+// @ts-ignore
+const page = computed(() => $slidev.nav.currentPage)
+const showPage = computed(() => {
+    return page.value !== 1 && page.value !== count + 1
+})
+</script>
+
+<style scoped>
+.cur-page {
+    color: #aaa;
+    position: absolute;
+    bottom: 1rem;
+    right: 2rem;
+    font-weight: bold;
+    filter: brightness(0) invert(1);
+    mix-blend-mode: difference;
+}
+</style>
