2087a463a1c23064bf348143f560c1629eea5a06
Mufanc <mufanc.xyz@gmail.com>
2024-01-01 13:50:20 +0800

    feat: 更新页码样式

--- global-top.vue
+++ global-top.vue
@@ -7,21 +7,17 @@
 </template>
 
 <script setup lang="ts">
-import { SlidevContextNav, SlidevContextNavClicks } from '@slidev/client/modules/context'
+import { SlidevContextNav } from '@slidev/client/modules/context'
 import { computed, UnwrapNestedRefs } from 'vue'
 
 // @ts-ignore
-const nav: UnwrapNestedRefs<SlidevContextNav & SlidevContextNavClicks> = $slidev.nav
+const nav: UnwrapNestedRefs<SlidevContextNav> = $slidev.nav
 
 const page = computed(() => nav.currentPage)
 const pageCount = computed(() => nav.total)
 
 const showPage = computed(() => {
     return page.value !== 1 && page.value !== pageCount.value + 1
-})
-
-const progress = computed(() => {
-    return `${((nav.clicks + 1) / (nav.clicksTotal + 1)) * 100}%`
 })
 </script>
 
@@ -30,27 +26,12 @@ const progress = computed(() => {
     position: absolute;
     bottom: 1rem;
     right: 2rem;
+    color: var(--color-primary);
     font-weight: bold;
-    filter: brightness(0);
-    mix-blend-mode: difference;
-
-    &::after {
-        @scale: 1.2;
-        @size: calc(0.7em / @scale);
-        content: '';
-        display: inline-block;
-        width: @size;
-        height: @size;
-        margin-left: 0.5em;
-        border-radius: @size;
-        border: 1px solid;
-        background: conic-gradient(black v-bind(progress), transparent v-bind(progress));
-        transition: all 0.2s ease-in-out;
-        transform: scale(@scale);
-    }
 }
 
 .page-count {
-    opacity: 0.2;
+    color: var(--color-secondary);
+    opacity: 0.5;
 }
 </style>
